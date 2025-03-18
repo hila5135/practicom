2688ee422885b6e4170f1f5acad7154d0c2b62fe
hila5135 <hilabassis@gmail.com>
2025-03-18 21:09:50 +0200

    without components

---
+++ AudioLectures-Client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import { RouterProvider } from 'react-router-dom'
+// import myRouter from './Router'
+// import React from 'react'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ AudioLectures-Client/src/Router.tsx
@@ -0,0 +1,20 @@
+// import { createBrowserRouter } from "react-router"
+// // import HomePage from "./components/HomePage"
+// // import AppLayout from "./components/AppLayout"
+
+
+
+
+// export const myRouter = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <AppLayout />,
+//         errorElement: <>main error</>,
+//         children: [
+            
+//         ]
+//     }
+
+// ])
+// export default myRouter;
+
