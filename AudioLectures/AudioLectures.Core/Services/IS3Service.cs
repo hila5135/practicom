9d0647e44892127e28d914a4cd3cc5166550c3cc
hila5135 <hilabassis@gmail.com>
2025-03-25 17:21:49 +0200

    adding delete

--- AudioLectures/AudioLectures.Core/Services/IS3Service.cs
+++ AudioLectures/AudioLectures.Core/Services/IS3Service.cs
@@ -8,8 +8,9 @@ namespace AudioLectures.Core.Services
 {
     public interface IS3Service
     {
-    Task<string> UploadFileAsync(Stream stream, string fileName);
-    Task<Stream> DownloadFileAsync(string fileName);
-    Task<List<string>> ListFilesAsync();
+        Task<string> UploadFileAsync(Stream stream, string fileName);
+        Task<Stream> DownloadFileAsync(string fileName);
+        Task<List<string>> ListFilesAsync();
+        Task<bool> DeleteFileAsync(string fileName);
     }
 }
--- AudioLectures/AudioLectures.Service/S3Service.cs
+++ AudioLectures/AudioLectures.Service/S3Service.cs
@@ -15,71 +15,80 @@ namespace AudioLectures.Service
 {
     public class S3Service : IS3Service
     {
-    private readonly AmazonS3Client _s3Client;
-    private readonly string _bucketName;
+        private readonly AmazonS3Client _s3Client;
+        private readonly string _bucketName;
 
-    public S3Service(IConfiguration configuration)
-    {
-      _bucketName = configuration["AWS:S3:BucketName"]; // שם ה-Bucket מתוך appsettings.json
-      string accessKey = configuration["AWS:AccessKey"];
-      string secretKey = configuration["AWS:SecretKey"];
-      string region = configuration["AWS:Region"];
+        public S3Service(IConfiguration configuration)
+        {
+            _bucketName = configuration["AWS:S3:BucketName"];
+            string accessKey = configuration["AWS:AccessKey"];
+            string secretKey = configuration["AWS:SecretKey"];
+            string region = configuration["AWS:Region"];
 
-      _s3Client = new AmazonS3Client(accessKey, secretKey, RegionEndpoint.GetBySystemName(region));
-    }
+            _s3Client = new AmazonS3Client(accessKey, secretKey, RegionEndpoint.GetBySystemName(region));
+        }
 
-    // ✅ פונקציה להעלאת קובץ ל-S3
-    public async Task<string> UploadFileAsync(Stream fileStream, string fileName)
-    {
-      var request = new TransferUtilityUploadRequest
-      {
-        InputStream = fileStream,
-        Key = fileName,
-        BucketName = _bucketName,
-        CannedACL = S3CannedACL.PublicRead // מאפשר גישה ציבורית לקובץ (לא חובה)
-      };
+        public async Task<string> UploadFileAsync(Stream fileStream, string fileName)
+        {
+            var request = new TransferUtilityUploadRequest
+            {
+                InputStream = fileStream,
+                Key = fileName,
+                BucketName = _bucketName,
+                CannedACL = S3CannedACL.PublicRead 
+            };
 
-      var transferUtility = new TransferUtility(_s3Client);
-      await transferUtility.UploadAsync(request);
+            var transferUtility = new TransferUtility(_s3Client);
+            await transferUtility.UploadAsync(request);
 
-      return $"https://{_bucketName}.s3.amazonaws.com/{fileName}";
-    }
+            return $"https://{_bucketName}.s3.amazonaws.com/{fileName}";
+        }
 
-    // ✅ פונקציה להורדת קובץ מ-S3
-    public async Task<Stream> DownloadFileAsync(string fileName)
-    {
-      var request = new GetObjectRequest
-      {
-        BucketName = _bucketName,
-        Key = fileName
-      };
+        public async Task<Stream> DownloadFileAsync(string fileName)
+        {
+            var request = new GetObjectRequest
+            {
+                BucketName = _bucketName,
+                Key = fileName
+            };
 
-      var response = await _s3Client.GetObjectAsync(request);
-      return response.ResponseStream;
-    }
+            var response = await _s3Client.GetObjectAsync(request);
+            return response.ResponseStream;
+        }
 
-    // ✅ פונקציה לקבלת רשימת הקבצים ב-S3
-    public async Task<List<string>> ListFilesAsync()
-    {
-      var request = new ListObjectsV2Request
-      {
-        BucketName = _bucketName
-      };
+        public async Task<List<string>> ListFilesAsync()
+        {
+            var request = new ListObjectsV2Request
+            {
+                BucketName = _bucketName
+            };
 
-      var response = await _s3Client.ListObjectsV2Async(request);
-      var fileNames = new List<string>();
+            var response = await _s3Client.ListObjectsV2Async(request);
+            var fileNames = new List<string>();
 
-      foreach (var obj in response.S3Objects)
-      {
-        fileNames.Add(obj.Key);
-      }
+            foreach (var obj in response.S3Objects)
+            {
+                fileNames.Add(obj.Key);
+            }
 
-      return fileNames;
-    }
-    //public async Task<IActionResult> DeleteFileAsync(string fileName)
-    //    {
-
-    //    }
-
+            return fileNames;
+        }
+        public async Task<bool> DeleteFileAsync(string fileName)
+        {
+            try
+            {
+                var request = new DeleteObjectRequest
+                {
+                    BucketName = _bucketName,
+                    Key = fileName
+                };
+                var response = await _s3Client.DeleteObjectAsync(request);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
