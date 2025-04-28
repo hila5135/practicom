565269f228b45ddf72921da20cad41707f179785
hila5135 <hilabassis@gmail.com>
2025-04-28 16:55:52 +0300

    after installing nuget

--- AudioLectures/AudioLectures.Api/Program.cs
+++ AudioLectures/AudioLectures.Api/Program.cs
@@ -1,3 +1,178 @@
+//using Amazon.Runtime;
+//using Amazon;
+//using Amazon.S3;
+//using AudioLectures.Core;
+//using AudioLectures.Core.Repositories;
+//using AudioLectures.Core.Services;
+//using AudioLectures.Data;
+//using AudioLectures.Data.Repositories;
+//using AudioLectures.Service;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.IdentityModel.Tokens;
+//using Microsoft.OpenApi.Models;
+
+//using System.Security.Claims;
+//using System.Text;
+//using System.Text.Json.Serialization;
+//using Microsoft.AspNetCore.Http.Features;
+//using Microsoft.EntityFrameworkCore;
+
+
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddControllers();
+
+//builder.Services.AddControllers().AddJsonOptions(options =>
+//{
+//    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+//    options.JsonSerializerOptions.WriteIndented = true;
+//});
+
+
+//builder.Services.AddEndpointsApiExplorer();
+
+
+////builder.Services.AddDbContext<DataContext>();
+
+
+
+//builder.Services.AddControllers();
+
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//    {
+//        Scheme = "Bearer",
+//        BearerFormat = "JWT",
+//        In = ParameterLocation.Header,
+//        Name = "Authorization",
+//        Description = "Enter your Bearer token",
+//        Type = SecuritySchemeType.Http
+//    });
+
+//    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+//    {
+//        {
+//            new OpenApiSecurityScheme
+//            {
+//                Reference = new OpenApiReference
+//                {
+//                    Type = ReferenceType.SecurityScheme,
+//                    Id = "Bearer"
+//                }
+//            },
+//            new string[] {}
+//        }
+//    });
+//    options.OperationFilter<FileUploadOperationFilter>();
+//});
+
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAll",
+//        policy => policy.AllowAnyOrigin()
+//                        .AllowAnyMethod()
+//                        .AllowAnyHeader());
+//});
+
+
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+//builder.Services.AddScoped<ILessonService, LessonService>();
+//builder.Services.AddScoped<IUserService, UserService>();
+//builder.Services.AddScoped<ILecturerService, LecturerService>();
+//builder.Services.AddScoped<ILessonRepository, LessonRepository>();
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+//builder.Services.AddScoped<ILecturerRepository, LecturerRepository>();
+
+//builder.Services.Configure<FormOptions>(options =>
+//{
+//    options.MultipartBodyLengthLimit = 100_000_000; // 100MB לדוגמה
+//});
+//builder.Services.AddSingleton<IAmazonS3>(sp =>
+//{
+//  var config = sp.GetRequiredService<IConfiguration>();
+//  var awsAccessKeyId = config["AWS:AccessKeyId"];
+//  var awsSecretAccessKey = config["AWS:SecretAccessKey"];
+//  var region = RegionEndpoint.USEast1;  // בחר את ה-region שלך
+
+//  var credentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey);
+//  return new AmazonS3Client(credentials, region);
+//});
+//builder.Services.AddScoped<AuthService>();
+//builder.Services.AddScoped<IS3Service, S3Service>();
+
+//builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//.AddJwtBearer(options =>
+//{
+//    options.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuer = true,
+//        ValidateAudience = true,
+//        ValidateLifetime = true,
+//        ValidateIssuerSigningKey = true,
+//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//        ValidAudience = builder.Configuration["Jwt:Audience"],
+//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+//        RoleClaimType = ClaimTypes.Role
+//    };
+//});
+
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+//    options.AddPolicy("EditorOrAdmin", policy => policy.RequireRole("Editor", "Admin"));
+//    options.AddPolicy("ViewerOnly", policy => policy.RequireRole("Viewer"));
+//});
+
+//builder.Services.AddDbContext<DataContext>(options =>
+//    options.UseMySql(
+//        builder.Configuration.GetConnectionString("DefaultConnection"),
+//        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+//    )
+//);
+
+//var app = builder.Build();
+
+////if (app.Environment.IsDevelopment())
+////{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+////}
+
+//app.UseHttpsRedirection();
+
+//app.Use(async (context, next) =>
+//{
+//    await next(); 
+
+//    if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+//    {
+//        context.Response.ContentType = "application/json";
+//        await context.Response.WriteAsync("{\"error\": \"You do not have permission to perform this action!\"}");
+//    }
+//    else if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+//    {
+//        context.Response.ContentType = "application/json";
+//        await context.Response.WriteAsync("{\"error\": \"You must be logged in to access this resource!\"}");
+//    }
+//});
+//app.UseCors("AllowAll");
+
+//app.UseAuthentication(); 
+//app.UseAuthorization();
+
+//app.MapControllers();
+//app.MapGet("/", () => "Welcome to Audio Lectures API!");
+//app.Run();
+
+
 using Amazon.Runtime;
 using Amazon;
 using Amazon.S3;
@@ -10,32 +185,28 @@ using AudioLectures.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-
 using System.Security.Claims;
 using System.Text;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
 
-builder.Services.AddControllers().AddJsonOptions(options =>
+// הרשמות לשירותים
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
+builder.Services.Configure<FormOptions>(options =>
 {
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    options.JsonSerializerOptions.WriteIndented = true;
+    options.MultipartBodyLengthLimit = 100_000_000; // 100MB
 });
 
-
 builder.Services.AddEndpointsApiExplorer();
-
-
-//builder.Services.AddDbContext<DataContext>();
-
-
-
-builder.Services.AddControllers();
 
 builder.Services.AddSwaggerGen(options =>
 {
@@ -66,7 +237,6 @@ builder.Services.AddSwaggerGen(options =>
     options.OperationFilter<FileUploadOperationFilter>();
 });
 
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -75,32 +245,24 @@ builder.Services.AddCors(options =>
                         .AllowAnyHeader());
 });
 
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ILessonService, LessonService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ILecturerService, LecturerService>();
 builder.Services.AddScoped<ILessonRepository, LessonRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ILecturerRepository, LecturerRepository>();
-
-builder.Services.Configure<FormOptions>(options =>
-{
-    options.MultipartBodyLengthLimit = 100_000_000; // 100MB לדוגמה
-});
-builder.Services.AddSingleton<IAmazonS3>(sp =>
-{
-  var config = sp.GetRequiredService<IConfiguration>();
-  var awsAccessKeyId = config["AWS:AccessKeyId"];
-  var awsSecretAccessKey = config["AWS:SecretAccessKey"];
-  var region = RegionEndpoint.USEast1;  // בחר את ה-region שלך
-
-  var credentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey);
-  return new AmazonS3Client(credentials, region);
-});
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<IS3Service, S3Service>();
+
+builder.Services.AddSingleton<IAmazonS3>(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    var awsAccessKeyId = config["AWS:AccessKeyId"];
+    var awsSecretAccessKey = config["AWS:SecretAccessKey"];
+    var region = RegionEndpoint.USEast1;
+    var credentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey);
+    return new AmazonS3Client(credentials, region);
+});
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
@@ -131,16 +293,7 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy("ViewerOnly", policy => policy.RequireRole("Viewer"));
 });
 
-
-var app = builder.Build();
-
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
-
-app.UseHttpsRedirection();
+// !!!!! הוספת ה-DbContext חייבת להיות לפני builder.Build()
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
@@ -148,9 +301,17 @@ builder.Services.AddDbContext<DataContext>(options =>
     )
 );
 
+var app = builder.Build();
+
+// קונפיגורציה ל-Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
 app.Use(async (context, next) =>
 {
-    await next(); 
+    await next();
 
     if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
     {
@@ -163,11 +324,13 @@ app.Use(async (context, next) =>
         await context.Response.WriteAsync("{\"error\": \"You must be logged in to access this resource!\"}");
     }
 });
+
 app.UseCors("AllowAll");
 
-app.UseAuthentication(); 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 app.MapGet("/", () => "Welcome to Audio Lectures API!");
+
 app.Run();
