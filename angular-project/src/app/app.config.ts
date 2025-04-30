e4efb3a3f4ab68ccb37c98dd4c16bd09c6708fe9
hila5135 <hilabassis@gmail.com>
2025-04-30 19:51:29 +0300

    after setting in angular

--- angular-project/src/app/app.config.ts
+++ angular-project/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { API_BASE_URL } from './api/api-client' // עדכני לפי הנתיב האמיתי
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-
+import { environment } from '../../environment';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideHttpClient(),
+{provide : API_BASE_URL , useValue: environment.apiUrl}],
 };
