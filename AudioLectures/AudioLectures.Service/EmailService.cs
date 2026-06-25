590b3144827e6b9610aaaaa467c162c979a76c01
hila5135 <hilabassis@gmail.com>
2026-06-25 19:45:41 +0300

    fix commit2

--- AudioLectures/AudioLectures.Service/EmailService.cs
+++ AudioLectures/AudioLectures.Service/EmailService.cs
@@ -1,82 +1,67 @@
-﻿using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using AudioLectures.Core.Models;
-using System.Runtime;
+﻿//using Microsoft.Extensions.Options;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Mail;
+//using System.Net;
+//using System.Text;
+//using System.Threading.Tasks;
+//using AudioLectures.Core.Models;
+//using System.Runtime;
 
-namespace AudioLectures.Service
-{
-    public class EmailService
-    {
-        private readonly EmailSettings _emailSettings;
+//namespace AudioLectures.Service
+//{
+//    public class EmailService
+//    {
+//        private readonly EmailSettings _emailSettings;
 
-        public EmailService(IOptions<EmailSettings> emailSettings)
-        {
-            _emailSettings = emailSettings.Value;
-        }
+//        public EmailService(IOptions<EmailSettings> emailSettings)
+//        {
+//            _emailSettings = emailSettings.Value;
+//        }
 
-        //public async Task SendEmailAsync(string to, string subject, string body)
-        //{
-        //    var mail = new MailMessage();
-        //    mail.From = new MailAddress(_emailSettings.SenderEmail, _emailSettings.SenderName); ;
-        //    mail.To.Add(to);
-        //    mail.Subject = subject;
-        //    mail.Body = body;
-        //    mail.IsBodyHtml = true;
+        
+//        public async Task SendEmailAsync(string to, string subject, string body)
+//        {
+//            try
+//            {
+//                var mail = new MailMessage();
 
-        //    using var smtp = new SmtpClient(_emailSettings.SmtpServer, _emailSettings.Port)
-        //    {
-        //        Credentials = new NetworkCredential(_emailSettings.SenderEmail, _emailSettings.Password),
-        //        EnableSsl = true
-        //    };
-
-        //    await smtp.SendMailAsync(mail);
-        //}
-        public async Task SendEmailAsync(string to, string subject, string body)
-        {
-            try
-            {
-                var mail = new MailMessage();
-
-                mail.From = new MailAddress(
-                    _emailSettings.SenderEmail,
-                    _emailSettings.SenderName
-                );
+//                mail.From = new MailAddress(
+//                    _emailSettings.SenderEmail,
+//                    _emailSettings.SenderName
+//                );
 
 
-                mail.To.Add(to);
-                mail.Subject = subject;
-                mail.Body = body;
-                mail.IsBodyHtml = true;
 
-                using var smtp = new SmtpClient(
-                    _emailSettings.SmtpServer,
-                    _emailSettings.Port
-                )
-                {
-                    Credentials = new NetworkCredential(
-                        _emailSettings.SenderEmail,
-                        _emailSettings.Password
-                    ),
-                    EnableSsl = true
-                };
+//                mail.To.Add(to);
+//                mail.Subject = subject;
+//                mail.Body = body;
+//                mail.IsBodyHtml = true;
 
-                //await smtp.SendMailAsync(mail);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("EMAIL ERROR:");
-                //Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.ToString());
-                Console.WriteLine(_emailSettings.SenderEmail);
-                Console.WriteLine(_emailSettings.SmtpServer);
-                Console.WriteLine(_emailSettings.Port);
-            }
-        }
-    }
-}
+//                using var smtp = new SmtpClient(
+//                    _emailSettings.SmtpServer,
+//                    _emailSettings.Port
+//                )
+//                {
+//                    Credentials = new NetworkCredential(
+//                        _emailSettings.SenderEmail,
+//                        _emailSettings.Password
+//                    ),
+//                    EnableSsl = true
+//                };
+
+//                //await smtp.SendMailAsync(mail);
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("EMAIL ERROR:");
+//                //Console.WriteLine(ex.Message);
+//                Console.WriteLine(ex.ToString());
+//                Console.WriteLine(_emailSettings.SenderEmail);
+//                Console.WriteLine(_emailSettings.SmtpServer);
+//                Console.WriteLine(_emailSettings.Port);
+//            }
+//        }
+//    }
+//}
